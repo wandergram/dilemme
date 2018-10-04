deb67c72b4f503a552dc44337f33a892aa24de97
Alexandra K <alexandrak@MacBook-de-Alexandra.local>
2018-10-04 15:38:31 +0200

    base files

---
+++ config.js
@@ -0,0 +1,348 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1538658028254,
+  "lastModified": 1538658028254,
+  "path": "./data/indice_dilemme_1.csv",
+  "reader": "csv",
+  "ddfPath": "./data/indice_dilemme_1.csv"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Risque",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Risque",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "Appréciation",
+    "zoomedMax": 150,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 1,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "Importance",
+    "zoomedMax": 150,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "0": "#92e25c",
+     "25": "hsl(202.5, 80%, 55%)",
+     "50": "#82176d",
+     "75": "#e2c85c",
+     "100": "#4d4d4d",
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "indicator",
+    "which": "Cat"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "use": "property",
+    "which": "Risque"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [
+    {
+     "Risque": "Jur",
+     "trailStartTime": "0001"
+    },
+    {
+     "Risque": "Sec",
+     "trailStartTime": "0001"
+    }
+   ],
+   "size": {
+    "allow": {
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 2,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.85
+    ],
+    "fixBaseline": 0,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "use": "indicator",
+    "which": "Importance",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "size_label": {
+    "_important": false,
+    "allow": {
+     "names": [
+      "_default"
+     ],
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.33
+    ],
+    "fixBaseline": 0,
+    "scaleType": "ordinal",
+    "use": "constant",
+    "which": "_default",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 1200,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "T",
+   "end": "0007",
+   "endOrigin": null,
+   "endSelected": "0007",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "start": "0001",
+   "startOrigin": null,
+   "startSelected": "0001",
+   "step": 1,
+   "unit": "year",
+   "value": "0007"
+  }
+ },
+ "ui": {
+  "adaptMinMaxZoom": true,
+  "buttons": [
+   "colors",
+   "find",
+   "zoom",
+   "trails",
+   "lock",
+   "moreoptions",
+   "fullscreen",
+   "presentation"
+  ],
+  "chart": {
+   "decorations": {
+    "enabled": true,
+    "xAxisGroups": null
+   },
+   "labels": {
+    "dragging": true,
+    "removeLabelBox": false
+   },
+   "lockNonSelected": "0007",
+   "margin": {
+    "left": 80.37,
+    "top": 0
+   },
+   "superhighlightOnMinimapHover": true,
+   "trails": true,
+   "whenHovering": {
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true
+   }
+  },
+  "cursorMode": "arrow",
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": true
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "size",
+    "colors",
+    "label",
+    "zoom",
+    "presentation",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "size",
+    "zoom",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find",
+    "size",
+    "zoom"
+   ]
+  },
+  "panWithArrow": false,
+  "presentation": false,
+  "show_ticks": true,
+  "splash": false,
+  "zoomOnScrolling": false
+ },
+ "chartType": "BubbleChart"
+};
